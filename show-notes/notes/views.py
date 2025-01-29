f8517bcd6367694408a710643a1590f971c797ea
samira dorchalian <samiradorchalain1998@gmail.com>
2025-01-29 17:38:18 +0330

    save methode in database - and get all info from database

--- show-notes/notes/views.py
+++ show-notes/notes/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
+from django.http import HttpResponse
+from .models import Note
 
 # Create your views here.
 
 def notes_list_view(request):
-    return render(request, 'notes/notes_list.html')
+    notes = Note.objects.all()
+    context = {
+        'notes_list': notes,
+    }
+    return render(request, 'notes/notes_list.html',context)
