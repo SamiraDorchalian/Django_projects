6afbd51b899793fb3272154d989b196dc5422285
samira dorchalian <samiradorchalain1998@gmail.com>
2025-01-29 18:33:15 +0330

    learning testing code

--- show-notes/notes/tests.py
+++ show-notes/notes/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.shortcuts import reverse
+from .models import Note
 
 # Create your tests here.
+class NotesListViewTest(TestCase):
+    def setUp(self):
+        self.note_obj = Note.objects.create(author='samira', text='Sample text.')
+
+    def test_notes_list_view_url(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_notes_list_view_url_by_name(self):
+        response = self.client.get(reverse('notes_list'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_notes_list_page(self):
+
+        response = self.client.get(reverse('notes_list'))
+        self.assertContains(response, self.note_obj.text)
--- show-notes/notes/urls.py
+++ show-notes/notes/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.notes_list_view,name='notes_list'),
+    path('', views.notes_list_view,name='notes_list'),
 ]
