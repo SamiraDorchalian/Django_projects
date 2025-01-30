664742ab70df22f9efb4de2c223e1c74e9869ea4
samira dorchalian <samiradorchalain1998@gmail.com>
2025-01-30 14:28:04 +0330

    settings panel admin

--- blog_website/blog/admin.py
+++ blog_website/blog/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from .models import Post
 # Register your models here.
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status', 'datetime_modified',  )
+    # ordering = ('status', )
+    ordering = ('-status', )
+
+# admin.site.register(Post, PostAdmin)
