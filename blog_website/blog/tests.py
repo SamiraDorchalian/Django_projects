489746c8209e5ff58962b4516c2472e93db35fe7
samira dorchalian <samiradorchalain1998@gmail.com>
2025-01-31 17:42:33 +0330

    write more testing code

--- blog_website/blog/tests.py
+++ blog_website/blog/tests.py
@@ -25,3 +25,25 @@ class BlogPostTest(TestCase):
         response = self.client.get(reverse('posts_list'))
         self.assertEqual(response.status_code, 200)
 
+    def test_post_title_on_blog_list_page(self):
+        response = self.client.get(reverse('posts_list'))
+        self.assertContains(response, self.post1.title)
+
+    def test_post_detail_url(self):
+        response = self.client.get(f'/blog/{self.post1.id}/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_detail_url_by_name(self):
+        response = self.client.get(reverse('posts_detail', args=[self.post1.id]))
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_details_on_blog_detail_page(self):
+        # response = self.client.get('/blog/1/')
+        response = self.client.get(reverse('posts_detail', args=[self.post1.id]))
+        self.assertContains(response, self.post1.title)
+        self.assertContains(response, self.post1.text)
+
+    def test_status_404_is_post_id_not_exist(self):
+        response = self.client.get(reverse('posts_detail', args=[999]))
+        self.assertEqual(response.status_code, 404)
+
