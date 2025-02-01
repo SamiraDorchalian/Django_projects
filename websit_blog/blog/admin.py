93501c992eab1e1b7259c686e718cf94803c1907
samira dorchalian <samiradorchalain1998@gmail.com>
2025-02-01 16:05:08 +0330

    settings panel admin --- show tabel head and chenge ordering

--- websit_blog/blog/admin.py
+++ websit_blog/blog/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Post
 
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status', 'datetime_created', 'datetime_modified', 'author', )
+    ordering = ('status', )
+
 # Register your models here.
-admin.site.register(Post)
+# admin.site.register(Post, PostAdmin)
+
