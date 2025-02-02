2d0619fb9e5f79e53edcbc302bceea7815f04a5f
samira dorchalian <samiradorchalain1998@gmail.com>
2025-02-02 17:16:42 +0330

    write test_draft_post_not_show_in_posts_list & using filter method instead of all

--- websit_blog/blog/views.py
+++ websit_blog/blog/views.py
@@ -8,7 +8,9 @@ from .models import Post
 # Create your views here.
 
 def post_list_view(request):
-    posts_list = Post.objects.all()
+    # posts_list = Post.objects.all()
+    posts_list = Post.objects.filter(status='pub')
+
     return render(request, 'blog/posts_list.html', {'posts_list' : posts_list})
 
 def post_detail_view(request, pk):
