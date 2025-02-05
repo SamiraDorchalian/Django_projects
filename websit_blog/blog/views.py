0f42393d214a7f613253b409075900c015b4f706
samira dorchalian <samiradorchalain1998@gmail.com>
2025-02-05 18:12:24 +0330

    make view for class_based ListView

--- websit_blog/blog/views.py
+++ websit_blog/blog/views.py
@@ -3,17 +3,26 @@ from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
+from django.views import generic
 
 from .models import Post
 from .forms import NewPostForm
 
 # Create your views here.
 
-def post_list_view(request):
-    # posts_list = Post.objects.all()
-    posts_list = Post.objects.filter(status='pub').order_by('-datetime_modified')
+# def post_list_view(request):
+#     # posts_list = Post.objects.all()
+#     posts_list = Post.objects.filter(status='pub').order_by('-datetime_modified')
+#
+#     return render(request, 'blog/posts_list.html', {'posts_list' : posts_list})
 
-    return render(request, 'blog/posts_list.html', {'posts_list' : posts_list})
+class PostListView(generic.ListView):
+    # model = Post
+    template_name = 'blog/posts_list.html'
+    context_object_name = 'posts_list'
+
+    def get_queryset(self):
+        return Post.objects.filter(status='pub').order_by('-datetime_modified')
 
 def post_detail_view(request, pk):
     post = get_object_or_404(Post, pk=pk)
