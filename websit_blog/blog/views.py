e167f7ecc4d0c06efe30761d868a26d5e40e1994
samira dorchalian <samiradorchalain1998@gmail.com>
2025-02-05 16:13:22 +0330

    make button for update - using mathod order for order posts how we want

--- websit_blog/blog/views.py
+++ websit_blog/blog/views.py
@@ -11,7 +11,7 @@ from .forms import NewPostForm
 
 def post_list_view(request):
     # posts_list = Post.objects.all()
-    posts_list = Post.objects.filter(status='pub')
+    posts_list = Post.objects.filter(status='pub').order_by('-datetime_modified')
 
     return render(request, 'blog/posts_list.html', {'posts_list' : posts_list})
 
@@ -58,6 +58,7 @@ def post_update_view(request, pk):
 
     if form.is_valid():
         form.save()
+        return redirect('posts_list')
 
 
     return render(request, 'blog/post_create.html', context={'form': form})
