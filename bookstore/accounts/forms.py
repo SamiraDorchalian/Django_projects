924319fe7946bb43f5ecbacf295eecf2d91ce997
samira dorchalian <samiradorchalain1998@gmail.com>
2025-02-11 12:31:36 +0330

    crreate app accounts - and write custom user form

---
+++ bookstore/accounts/forms.py
@@ -0,0 +1,19 @@
+# UserCreateForm -> SignUp
+# UserChangeForm -> Admin
+
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+from .models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = UserCreationForm.Meta.fields + ('age', )
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = CustomUser
+        fields = UserChangeForm.Meta.fields
+
+
