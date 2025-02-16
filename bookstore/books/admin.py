13d6f4c3b40394d020becb718b29aa341938fb23
samira dorchalian <samiradorchalain1998@gmail.com>
2025-02-16 16:07:40 +0330

    add user to book model who created the book object

--- bookstore/books/admin.py
+++ bookstore/books/admin.py
@@ -4,7 +4,7 @@ from .models import Book,Comment
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', )
+    list_display = ('title', 'author', 'user', )
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
