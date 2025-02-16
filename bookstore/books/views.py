46aa5e9b5e5354586909531aef572d059d6ca3de
samira dorchalian <samiradorchalain1998@gmail.com>
2025-02-16 16:37:53 +0330

    add permissions

--- bookstore/books/views.py
+++ bookstore/books/views.py
@@ -1,17 +1,19 @@
 from django.views import generic
 from django.urls import reverse_lazy
 from django.shortcuts import get_object_or_404, render
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.decorators import login_required
 
 from .models import Book
 from .forms import CommentForm
+
 
 class BookListView(generic.ListView):
     model = Book
     paginate_by = 4
     template_name = 'books/book_list.html'
     context_object_name = 'books'
+
 
 # class BookDetailView(generic.DetailView):
 #     model = Book
@@ -37,23 +39,32 @@ def book_detail_view(request, pk):
     book_comments = book.comments.all()
     return render(request, 'books/book_detail.html', {
         'book': book,
-        'comments':book_comments,
+        'comments': book_comments,
         'comment_form': comment_form,
     })
+
 
 class BookCreateView(LoginRequiredMixin, generic.CreateView):
     model = Book
     fields = ['title', 'author', 'description', 'price', 'cover', ]
     template_name = 'books/book_create.html'
 
-class BookUpdateView(LoginRequiredMixin, generic.UpdateView):
+
+class BookUpdateView(LoginRequiredMixin, UserPassesTestMixin, generic.UpdateView):
     model = Book
     fields = ['title', 'author', 'description', 'cover', ]
     template_name = 'books/book_update.html'
 
+    def test_func(self):
+        obj = self.get_object()
+        return obj.user == self.request.user
 
-class BookDeleteView(LoginRequiredMixin, generic.DeleteView):
+
+class BookDeleteView(LoginRequiredMixin, UserPassesTestMixin, generic.DeleteView):
     model = Book
     template_name = 'books/book_delete.html'
     success_url = reverse_lazy('book_list')
 
+    def test_func(self):
+        obj = self.get_object()
+        return obj.user == self.request.user
