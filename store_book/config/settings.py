f1c5e5c700771f357bff266bf18ced2ce8d4996f
samira dorchalian <samiradorchalain1998@gmail.com>
2025-02-24 16:35:25 +0330

    write some test & install bootstrap5 vs Cispy Forms

--- store_book/config/settings.py
+++ store_book/config/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # packages
+    "crispy_forms",
+    "crispy_bootstrap5",
+
     # local
     "accounts",
     'pages',
@@ -131,4 +135,5 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
-
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5
