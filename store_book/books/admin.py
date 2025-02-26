c6405751333a6351cc2955c405017ab840e1c39b
samira dorchalian <samiradorchalain1998@gmail.com>
2025-02-26 17:37:38 +0330

    add related to book fk in comment

--- store_book/books/admin.py
+++ store_book/books/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Book, Comment
 
-admin.site.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title','author','publisher','translator','price','pages_book', 'year_of_publication', 'cover', )
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'book', 'text', 'datetime_created', )
+
+admin.site.register(Book, BookAdmin)
+# admin.site.register(Comment, CommentAdmin)
+
+
 
