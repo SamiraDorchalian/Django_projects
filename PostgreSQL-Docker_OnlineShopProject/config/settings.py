a8958c96664883f0f5ba3e8eb860c37f5c6b24b5
samira dorchalian <samiradorchalain1998@gmail.com>
2025-04-29 00:21:11 +0330

    add postgresql and django project to docker & create accounts app

--- PostgreSQL-Docker_OnlineShopProject/config/settings.py
+++ PostgreSQL-Docker_OnlineShopProject/config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cxro+i&_=79#e7yzuf*sk#(981sy(zl2iums6+n3hpt$(nf^@2'
+SECRET_KEY = 'django-insecure-k#9t*(dvj(*&9))@^%$d(!o@!^07&ulw6c0j=jsaqw*r#pmohu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -72,6 +74,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 DATABASES = {
     'default': {
@@ -130,3 +139,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#accounts config
+AUTH_USER_MODEL = 'accounts.CustomUser'
