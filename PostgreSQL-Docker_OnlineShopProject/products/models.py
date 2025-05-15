dbd2766588993e7beb361a162842a4167cfe3eb3
samira dorchalian <samiradorchalain1998@gmail.com>
2025-05-15 14:30:50 +0330

    add short_description field to product model

--- PostgreSQL-Docker_OnlineShopProject/products/models.py
+++ PostgreSQL-Docker_OnlineShopProject/products/models.py
@@ -7,8 +7,9 @@ from django.utils.translation import gettext_lazy as _
 from ckeditor.fields import RichTextField
 
 class Product(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=100)
     description = RichTextField()
+    short_description = models.TextField(blank=True)
     price = models.PositiveIntegerField(default=0)
     active = models.BooleanField(default=True)
     image = models.ImageField(verbose_name= _('Product Image'), upload_to='product/product_cover/', blank=True, )
